f7b0df766eb5f885231db67c7829e93b9364842c
MERDGN <e.mirayerdgn40@gmail.com>
2025-11-07 14:38:20 +0300

    feat: expo proje dosyaları eklendi

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import TimerScreen from './src/screens/TimerScreen';
+import ReportsScreen from './src/screens/ReportsScreen';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerTitleAlign: 'center' }}>
+        <Tab.Screen name="Zamanlayıcı" component={TimerScreen} />
+        <Tab.Screen name="Raporlar" component={ReportsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
